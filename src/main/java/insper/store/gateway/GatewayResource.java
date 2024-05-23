31ba22a6a0b40d4e4ed1ad73db4a23e401fb5e7d
WeeeverAlex <awever4000@gmail.com>
2024-05-22 23:10:58 -0300

    Individual Wever - Circuit Breaker

--- src/main/java/insper/store/gateway/GatewayResource.java
+++ src/main/java/insper/store/gateway/GatewayResource.java
@@ -2,6 +2,7 @@ package insper.store.gateway;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class GatewayResource {
@@ -11,4 +12,8 @@ public class GatewayResource {
         return "Hello from Gateway!";
     }
 
+    @GetMapping("/fallback")
+    public ResponseEntity<String> fallback() {
+        return ResponseEntity.ok("Serviço temporariamente indisponível. Tente novamente mais tarde.");
+    }
 }
